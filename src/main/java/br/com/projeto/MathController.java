cef7382f8e31e6ae7af92aec0cbc706d0bf8e63b
rafael <rafaelmarinhoalves5@gmail.com>
2024-01-10 22:03:04 -0300

    primeiro exercicio

--- src/main/java/br/com/projeto/MathController.java
+++ src/main/java/br/com/projeto/MathController.java
@@ -7,18 +7,53 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.projeto.exceptions.UnsupMathOp;
+
 @RestController
 public class MathController {
 
-	private AtomicLong Counter = new AtomicLong();
 	
-	@RequestMapping(value = "/sum/{numone}/{numtwo}" , method=RequestMethod.GET)
+	@RequestMapping(value = "/operation/{numone}/{operation}/{numtwo}" , method=RequestMethod.GET)
 	
-	public Double sum(
-					  @PathVariable(value = "numone")String numone,
-					  @PathVariable(value = "numtwo")String numtwo) {
-						return 1D;
+	public Double operation( @PathVariable(value = "numone")String numone, 
+			@PathVariable(value = "operation") String operation,
+			@PathVariable(value = "numtwo")String numtwo) throws Exception {
+		if(!IsNum(numone) || !IsNum(numtwo)){
+			throw new UnsupMathOp("not supported");
+		}
+		if(operation.equals("x")) {
+			return convDouble(numone) * convDouble(numtwo);
+		}
+		if(operation.equals("-")) {
+			return convDouble(numone) - convDouble(numtwo);
+		}
+		if(operation.equals("/")) {
+			return convDouble(numone) / convDouble(numtwo);
+		}
+		if(operation.equals("x2")) {
+			return (convDouble(numone) + convDouble(numtwo)) / 2;
+		}
+		if(operation.equals("+")) {
+			return convDouble(numone) + convDouble(numtwo);
+		}
+		else{
+			return 0D;
+		}
 		
+	}
+	
+	
+	private Double convDouble(String strnum) {
+		if(strnum == null) return 0D;
+		String num = strnum.replaceAll(",", ".");
+		if(IsNum(num)) return Double.parseDouble(num);
+		return 0D;
+	}
+
+	private boolean IsNum(String strnum) {
+		if(strnum == null) return false;
+		String num = strnum.replaceAll(",", ".");
+		return num.matches("[-+]?[0-9]*\\.?[0-9]+");
 	}
 
 }
